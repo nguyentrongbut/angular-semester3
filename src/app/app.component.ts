bdd5aff151c7c6be31c03f78262ae4970abb58ca
kyozun <daicacuong0311@gmail.com>
2024-03-11 09:35:46 +0700

    Init Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from "primeng/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import { RippleModule} from "primeng/ripple";
+import {PrimeNGConfig} from "primeng/api";
+import {MultiSelectModule} from "primeng/multiselect";
+import {FormsModule} from "@angular/forms";
+
+interface City {
+  name: string,
+  code: string
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule, RippleModule, MultiSelectModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'angular-semester3';
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  cities!: City[];
+
+  selectedCities!: City[];
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+    ];
+  }
+}
